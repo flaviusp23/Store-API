9b9175a49acb14bb09e60675b94eadf0249c4a3b
flaviusp23 <flavius.paltin@yahoo.com>
2024-09-02 20:22:25 +0300

    project setup

---
+++ app.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const connectDB = require('./db/connect');
+
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send('<h1>Store API</h1><a href="api/v1/products">products route</a>')
+})
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async() =>{
+    try{
+        app.listen(port, console.log(`Server is listening port ${port}...`))
+        await connectDB(process.env.MONGO_URL)
+    } catch(err){
+        console.log(err)
+    }
+}
+
+start()
