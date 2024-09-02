52ad51d980a8a1458454d1faa82fd298b3ff083c
flaviusp23 <flavius.paltin@yahoo.com>
2024-09-03 00:15:45 +0300

    fixed bugs

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,5 @@
 const Product = require('../models/product')
 
-const getAllProductsStatic = async (req,res) =>{
-    const products = await Product.find({})
-    res.status(200).json({products, nbHits: products.length});
-}
-
 const getAllProducts = async (req,res) =>{
     const { featured, company, name, sort, fields, minPrice, maxPrice, minRating, maxRating } = req.query;
     //filtering
@@ -72,5 +67,4 @@ const getAllProducts = async (req,res) =>{
 
 module.exports = {
     getAllProducts,
-    getAllProductsStatic,
 }
--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router()
 
-const {
-    getAllProducts,
-    getAllProductsStatic
-} = require('../controllers/products')
+const { getAllProducts } = require('../controllers/products')
 
 router.get('/',getAllProducts)
-router.get('/static',getAllProductsStatic)
 
 module.exports = router
